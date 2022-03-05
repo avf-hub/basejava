cd816d3961cf3eb9a8ba08554abe5df9b0096c22
Andrey Falkovskiy <avf-91@yandex.ru>
2022-03-05 21:40:33 +0300

    implement HW_5 part_1 (fix_1)

--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class AbstractStorageTest {
+public abstract class AbstractStorageTest {
     private Storage storage;
 
     private static final String UUID_1 = "uuid1";
