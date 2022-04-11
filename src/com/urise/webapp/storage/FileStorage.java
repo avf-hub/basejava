55dc2bdc6c3cd3211ed6a8cef587dc4e877654e4
Andrey Falkovskiy <avf-91@yandex.ru>
2022-04-11 23:00:05 +0300

    implement HW_9 (fix_3)

--- src/com/urise/webapp/storage/FileStorage.java
+++ src/com/urise/webapp/storage/FileStorage.java
@@ -72,10 +72,7 @@ public class FileStorage extends AbstractStorage<File> {
 
     @Override
     protected List<Resume> doCopyAll() {
-        File[] files = directory.listFiles();
-        if (files == null) {
-            throw new StorageException("Directory read error", null);
-        }
+        File[] files = getListFiles();
         List<Resume> list = new ArrayList<>(files.length);
         for (File file : files) {
             list.add(doGet(file));
@@ -85,11 +82,8 @@ public class FileStorage extends AbstractStorage<File> {
 
     @Override
     public void clear() {
-        File[] files = directory.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                doDelete(file);
-            }
+        for (File file : getListFiles()) {
+            doDelete(file);
         }
     }
 
@@ -100,5 +94,13 @@ public class FileStorage extends AbstractStorage<File> {
             throw new StorageException("Directory read error", null);
         }
         return list.length;
+    }
+
+    private File[] getListFiles() {
+        File[] files = directory.listFiles();
+        if (files == null) {
+            throw new StorageException("Directory read error", null);
+        }
+        return files;
     }
 }
