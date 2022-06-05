fed5808c67c88281639efebe9fc36f3803c47ace
Andrey Falkovskiy <avf-91@yandex.ru>
2022-06-05 11:17:59 +0300

    implement HW_12 (fix_4)

--- src/com/urise/webapp/HomeWork12.java
+++ src/com/urise/webapp/HomeWork12.java
@@ -8,13 +8,12 @@ import java.util.stream.Collectors;
 public class HomeWork12 {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 5, 4, 2, 6, 2, 9, 4, 8, 1, 3, 9};
-        System.out.println("\n" + minValue(numbers));
+        System.out.println(minValue(numbers));
         System.out.println(oddOrEven(Arrays.stream(numbers).collect(ArrayList::new, ArrayList::add, ArrayList::addAll)));
     }
 
     private static int minValue(int[] values) {
-        Arrays.stream(values).distinct().sorted().forEach(System.out::print);
-        return Arrays.stream(values).distinct().sorted().reduce(Integer.MAX_VALUE, Integer::min);
+        return Arrays.stream(values).distinct().sorted().reduce(0, (a, b) -> 10 * a + b);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
